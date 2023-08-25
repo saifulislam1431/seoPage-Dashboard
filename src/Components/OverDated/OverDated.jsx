e28a53ef8517183ad4064eca9b92f49d59146f1d
Saiful Islam <sakibbhaiya143@gmail.com>
2023-08-25 21:34:30 +0600

    Data fetch in all component

--- src/Components/OverDated/OverDated.jsx
+++ src/Components/OverDated/OverDated.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import CardTitleCompo from '../CardTitleCompo/CardTitleCompo';
 import logo from "../../assets/deadline.png"
+import ClientCards from '../ClientCards/ClientCards';
 const OverDated = () => {
     return (
-        <section className='bg-slate-200 rounded-md px-3 h-[800px] w-96 relative'>
+        <section className='bg-slate-200 rounded-md px-3 h-[800px] w-96 relative verticalScroll style-8 '>
         <div className='bg-slate-200 sticky top-0 left-0 right-0 py-2'>
 <CardTitleCompo logo={logo} title="Expire"/>
+</div>
+
+<div>
+    <ClientCards />
 </div>
                 </section>
     );
