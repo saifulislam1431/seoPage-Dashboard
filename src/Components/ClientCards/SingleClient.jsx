b0d8147094053f5eaf6c4842867ac1aab1bde93e
Saiful Islam <sakibbhaiya143@gmail.com>
2023-08-26 00:07:42 +0600

    Modal added

--- src/Components/ClientCards/SingleClient.jsx
+++ src/Components/ClientCards/SingleClient.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaClipboardList, FaLayerGroup, FaRegComments } from "react-icons/fa";
 import { HiOutlineCalendarDays, HiPaperClip } from "react-icons/hi2";
 import men from "../../assets/man.png";
 import women from "../../assets/woman.png";
+import useClients from '../../hooks/useClients';
+import "./Input.css"
 
 const SingleClient = ({singleClient}) => {
+
+    const[allClients,refetch] = useClients();
+    const [clientAttachments, setAttachments] = useState([]);
+
     const {author_img,author_name,client_img,client_name,comments,date,_id,attachments} = singleClient
+
+    const handleAttachment = (event) => {
+        const newAttachments = Array.from(event.target.files);
+        setAttachments(newAttachments);
+      };
+const handleAttach = (id) =>{
+    console.log(id);
+}
+
+
+
+
     return (
         <section className='bg-white rounded-md h-[180px] p-2'>
 <div className='flex items-center justify-between'>
@@ -37,13 +55,39 @@ const SingleClient = ({singleClient}) => {
   <FaRegComments className='w-5 h-5'/> <p className='font-semibold'>{comments}</p> 
 </div>
 <div className='inline-flex items-center gap-1'>
-    <HiPaperClip className='w-5 h-5'/>
+<label htmlFor={_id} className='cursor-pointer' ><HiPaperClip className='w-5 h-5'/></label>
+    
     <p className='font-semibold'>{attachments ? attachments.length : 0}</p>
 </div>
 <div className='inline-flex items-center gap-1'>
     <HiOutlineCalendarDays className='w-5 h-5'/>
     <p className='font-semibold'>{date}</p>
 </div>
+</div>
+<input type="checkbox" id={_id} className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box">
+  <div>
+
+            <h1 className='font-semibold text-center my-5 text-secondary'>Client Name: {client_name}</h1>
+       
+          <div>
+          <input type="file" multiple onChange={handleAttachment} className='my-4 inputFile'/>
+          </div>
+          
+          <div>
+          {clientAttachments.length > 0 ? clientAttachments.map((attachment, index) => (
+            <div key={index} className='my-2 font-medium'>{attachment.name}</div>
+          )) : <span className='my-7'>Select Your Attachment</span>}
+          </div>
+
+
+
+        </div>
+    <div className="modal-action">
+      <label htmlFor={_id} className="py-1 px-3 bg-secondary text-teal-50 rounded-md my-2 cursor-pointer" onClick={()=>{handleAttach(_id)}}>Save Attachments</label>
+    </div>
+  </div>
 </div>
         </section>
     );
