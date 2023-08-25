ef692e8a1960c6638a75160479890f7f79824ede
Saiful Islam <sakibbhaiya143@gmail.com>
2023-08-25 12:26:07 +0600

    Project config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,32 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#5d1cea",
+        
+"secondary": "#d37b32",
+        
+"accent": "#5bce3b",
+        
+"neutral": "#1e1825",
+        
+"base-100": "#ecf0f4",
+        
+"info": "#70a7f0",
+        
+"success": "#1c9b6b",
+        
+"warning": "#e7b613",
+        
+"error": "#fa0a3a",
+        },
+      },
+      "night"
+    ],
   },
   plugins: [require("daisyui")],
 }
